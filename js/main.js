c714a11105fb150ec9eeaf04e2b864672681f38e
Jellis <venetianself@hotmail.com>
2017-06-12 00:19:07 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-console.log("Up and running!");
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne;
-cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log()
-var cardTwo;
-cardTwo = cards[2];
-cardsInPlay.push("cardTwo");
-console.log()
 
-if (cardsInPlay.length === 2) {
-
-} else if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
 }
+}
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+
+
+
+
+
+
+
